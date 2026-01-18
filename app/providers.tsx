c5d83295874752ec5610fcb6a138d0d6bad15fef
Oluwaseyi-vibex <oluseyiwmwm@gmail.com>
2026-01-18 12:47:02 +0100

    feat: register page ui

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,11 +2,15 @@
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
-
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/auth/register");
+  });
   return (
     <QueryClientProvider client={queryClient}>
       {children}
